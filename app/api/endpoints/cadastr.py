c7c74a4d08e1d32ac5f32872655646e282adbf8d
Kapkaev Andrey <kapkaew@yandex.ru>
2023-09-23 23:04:45 +0300

    Many updates, change env configuration, fixed settings for pydantic and regex pattern. Connect all endpoints.

--- app/api/endpoints/cadastr.py
+++ app/api/endpoints/cadastr.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from app.api.schemas.cadastr import CadastrDataSchema, CadastrServiceResponse
-from app.core.worker import create_task
+from app.worker import create_task
 
 router = APIRouter()
 
@@ -14,10 +14,10 @@ router = APIRouter()
 @router.post("/cadastr/calculate", status_code=HTTPStatus.ACCEPTED)
 async def import_folders_and_files(
     items_data: CadastrDataSchema,
-) -> None:
+) -> CadastrServiceResponse:
     task_id = uuid4()
     create_task.delay(task_id)
-    await CadastrServiceResponse(result_id=task_id)
+    return CadastrServiceResponse(result_id=task_id)
 
 
 @router.get("/cadastr/result/{task_id}")
--- app/api/schemas/cadastr.py
+++ app/api/schemas/cadastr.py
@@ -1,11 +1,11 @@
 from pydantic import UUID4, BaseModel, Field
 
 from app.core.constants.base import Latitude, Longitude
-from app.core.constants.regexes import CADASTR_NUMBER_REGEX
+from app.core.constants.regexes import CADASTR_NUMBER_PATTERN, CADASTR_NUMBER_REGEX
 
 
 class CadastrDataSchema(BaseModel):
-    cadastral_number: str = Field(regex=CADASTR_NUMBER_REGEX)
+    cadastral_number: str = Field(pattern=CADASTR_NUMBER_PATTERN)
     latitude: float = Field(le=Latitude.MAX, ge=Latitude.MIN)
     longitude: float = Field(le=Longitude.MAX, ge=-Longitude.MIN)
 
