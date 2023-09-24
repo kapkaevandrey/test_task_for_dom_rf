8c5a2710de45e7b534cd9f53027900a5a6485b27
Kapkaev Andrey <kapkaew@yandex.ru>
2023-09-24 11:41:28 +0300

    Refactor worker and tasks.

--- app/worker.py
+++ app/worker.py
@@ -1,6 +1,3 @@
-import os
-import time
-
 from celery import Celery
 
 from app.core.settings import settings
@@ -9,8 +6,4 @@ celery = Celery(__name__)
 celery.conf.broker_url = settings.celery_broker_url
 celery.conf.result_backend = settings.celery_result_backend
 
-
-@celery.task(name="create_task")
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
-    return True
+celery.autodiscover_tasks(packages=["app"])
